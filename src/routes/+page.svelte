24f33b97a7755ac5353fd0c891574a9b36f10f4e
i.nafikov <i.nafikov@tinkoff.ru>
2023-04-15 20:59:27 +0300

    chore: run linter and edit hooks pre-commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
 </script>
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link
+	rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+/>
