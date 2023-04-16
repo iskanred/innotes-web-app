0c6ed1f5e66162bdcb455484894d6ba357e150d6
iskanred <nafikov.iskander@mail.ru>
2023-04-16 15:29:16 +0300

    feat: add svelte-ui and Home page

---
+++ src/lib/widgets/header/ui/HeaderAnchor.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Anchor } from '@svelteuidev/core';
+
+	export let href: string;
+
+	export let text: string;
+</script>
+
+<Anchor size={48} weight={'bold'} {href} underline={false}>
+	{text}
+</Anchor>
