d8c0c06832bb3e8cbb3c7fe6da9d89f8902c9820
i.nafikov <i.nafikov@tinkoff.ru>
2023-04-17 22:22:24 +0300

    fix: make content centered vertically on Home page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,22 +2,20 @@
 	import { Button, Text } from '@svelteuidev/core';
 </script>
 
-<main>
+<div id='content'>
 	<Text size={52} weight="extrabold">What is this?</Text>
 	<Text size={32}>This service is a simple notes taking web application.</Text>
 	<Button size="lg" ripple>Create your note</Button>
-</main>
+</div>
 
 <style>
-	main {
-		height: 100%;
+	#content {
+		height: 80vh;
 		width: 100%;
 
 		gap: 72px;
 
-        /* todo: center vertically */
-
-		/* center horizontally */
+        /* center horizontally */
 		display: flex;
 		justify-content: center;
 		flex-direction: column;
