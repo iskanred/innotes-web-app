b0fa73958b935eaba92158ef1f6a648822ee53a7
iskanred <nafikov.iskander@mail.ru>
2023-04-22 22:21:47 +0300

    feat: add github repo to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-	import { Button, Text, Title } from "@svelteuidev/core";
+	import { ActionIcon, Anchor, Button, Group, Text, Title } from "@svelteuidev/core";
+	import { GithubLogo } from "radix-icons-svelte";
+	import { REPOSITORY_LINK } from "$lib/shared/constants/model/Constants.js";
+	import { goto } from "$app/navigation";
+	import { notesPagePath } from "$lib/shared/path/model/Paths.js";
 </script>
 
 <main>
 	<Title order={1} size={52} weight="extrabold">What is this?</Title>
 	<Text size={32}>This service is a simple notes taking web application.</Text>
-	<Button size="lg" ripple>Create your note</Button>
+	<Button size="lg" ripple on:click={() => goto(notesPagePath)}>Create your note</Button>
+	<Group>
+		<ActionIcon root="a" href={REPOSITORY_LINK}>
+			<GithubLogo size={106} />
+		</ActionIcon>
+		<Anchor root="a" href={REPOSITORY_LINK}>GitHub Repository</Anchor>
+	</Group>
 </main>
 
 <style>
