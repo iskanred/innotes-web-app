65f9341ae3003a96ad1e1a8094c9b1434b6524fa
i.nafikov <i.nafikov@tinkoff.ru>
2023-04-22 21:11:13 +0300

    feat: add list of notes page

---
+++ src/routes/auth/+page.server.ts
@@ -0,0 +1,19 @@
+import { fail } from "@sveltejs/kit";
+import type { Actions } from "@sveltejs/kit";
+import { SESSION_COOKIE_NAME } from "$lib/shared/constants/model/Constants";
+
+export const actions: Actions = {
+	default: async ({ request, cookies }) => {
+		const formData = await request.formData();
+		const token = formData.get("token")?.valueOf();
+		if (!token || typeof token !== "string") {
+			return fail(400, { message: "Token is a required field and must be a string" });
+		}
+		cookies.set(SESSION_COOKIE_NAME, token, {
+			httpOnly: true,
+			path: "/",
+			secure: true
+		});
+		return { success: true };
+	}
+};
