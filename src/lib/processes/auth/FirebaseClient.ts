9e8ed373559085af1c3c23240eb51d25ce35a6c1
majorro <andrabaratrum007@gmail.com>
2023-04-22 18:53:12 +0300

    fixed env variables

--- src/lib/processes/auth/FirebaseClient.ts
+++ src/lib/processes/auth/FirebaseClient.ts
@@ -2,12 +2,12 @@ import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_APIKEY,
-	authDomain: import.meta.env.VITE_AUTHDOMAIN,
-	projectId: import.meta.env.VITE_PROJECTID,
-	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-	appId: import.meta.env.VITE_APPID
+	apiKey: import.meta.env.VITE_APIKEY || process.env.VITE_APIKEY,
+	authDomain: import.meta.env.VITE_AUTHDOMAIN || process.env.VITE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_PROJECTID || process.env.VITE_PROJECTID,
+	storageBucket: import.meta.env.VITE_STORAGEBUCKET || process.env.VITE_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID || process.env.VITE_MESSAGINGSENDERID,
+	appId: import.meta.env.VITE_APPID || process.env.VITE_APPID
 };
 
 // Initialize Firebase
