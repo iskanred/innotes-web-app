97cb90c23a2ea16c05b0d22cdd22cfdc45775920
i.nafikov <i.nafikov@tinkoff.ru>
2023-04-24 19:03:38 +0300

    feat: change adapter to cloudfare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 const dev = process.argv.includes("dev");
