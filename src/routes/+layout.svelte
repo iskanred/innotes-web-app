a992bdf292ad44eb53b6a7999d54ccdcf22722c4
quhaaST <asndjabbd@gmail.com>
2023-04-25 13:31:09 +0300

    Fixed site preview image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	<meta property="og:url" content={$page.url.toString()} />
 	<meta property="og:title" content={$page.data.title} />
 	<meta property="og:description" content={$page.data.content} />
-	<meta property="og:image" content="${base}/favicon.ico" />
+	<meta property="og:image" content="{base}/favicon.ico" />
 	<meta property="og:image:width" content="968" />
 	<meta property="og:image:height" content="504" />
 </svelte:head>
